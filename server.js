23b61335af010a0814289fedb0ae25e399771c14
Bilal Waheed <bilalwaheed099@hotmail.com>
2023-12-30 02:49:06 +0400

    hotfix

--- server.js
+++ server.js
@@ -35,6 +35,6 @@ app.use(passport.initialize());
 
 require('./config/passport')(passport);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {console.log(`Connected on port ${port}`)});
